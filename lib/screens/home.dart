5926f22bbed1781748dc6a218462ee89e86b2208
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-02 17:34:26 -0300

    Added cards to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,23 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:github/main.dart';
 import '../models/User.dart';
 
 class Home extends StatelessWidget {
-
   final Color mainColor = Color(0xff262626);
+
   final Color secondaryColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    User user = ModalRoute.of(context).settings.arguments;
+    User user = ModalRoute
+      .of(context)
+      .settings
+      .arguments;
 
     return Scaffold(
       body: Container(
@@ -36,7 +42,7 @@ class Home extends StatelessWidget {
                         ),
                       ),
                       Icon(
-                        Icons.search,
+                        Icons.exit_to_app,
                         size: 32.0,
                         color: Colors.white,
                       )
@@ -46,9 +52,9 @@ class Home extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30.0),
                   child: Container(
-                    width: 85.0,
-                    height: 85.0,
-                    margin: EdgeInsets.only(top: 50.0),
+                    width: 70.0,
+                    height: 70.0,
+                    margin: EdgeInsets.only(top: 40.0, bottom: 20.0),
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(image: NetworkImage(user.avatarUrl), fit: BoxFit.fill),
@@ -56,7 +62,7 @@ class Home extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(30.0, 25.0, 30.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
                   child: Text(
                     "Hello, " + user.name,
                     style: TextStyle(
@@ -78,19 +84,52 @@ class Home extends StatelessWidget {
                 Expanded(
                   child: Container(
                     child: PageView(
-                      controller: PageController(initialPage: 0, viewportFraction: 0.8),
+                      controller: PageController(initialPage: 0, viewportFraction: 0.9),
+                      scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        Container(
-                          color: Colors.pink,
-                          width: 150.0,
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Card(
+                              title: "Repositories",
+                              icon: Icons.folder_open,
+                              link: ""
+                            ),
+                          ),
+                          onTap: () {},
                         ),
-                        Container(
-                          color: Colors.cyan,
-                          width: 150.0,
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Card(
+                              title: "Subscriptions",
+                              icon: Icons.local_offer,
+                              link: ""
+                            ),
+                          ),
+                          onTap: () {},
                         ),
-                        Container(
-                          color: Colors.deepPurple,
-                          width: 150.0,
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Card(
+                              title: "Following",
+                              icon: Icons.people_outline,
+                              link: ""
+                            ),
+                          ),
+                          onTap: () {},
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Card(
+                              title: "Followers",
+                              icon: Icons.person_outline,
+                              link: ""
+                            ),
+                          ),
+                          onTap: () {},
                         ),
                       ],
                     ),
@@ -104,3 +143,55 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class Card extends StatelessWidget {
+
+  final String title, link;
+  final IconData icon;
+
+  const Card({Key key, this.title, this.link, this.icon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 50.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 8.0,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.grey[400]
+              )
+            ),
+            child: Icon(
+              this.icon,
+              color: Color(0xff262626),
+            ),
+          ),
+          Text(
+            this.title,
+            style: TextStyle(
+              fontSize: 35.0,
+              color: Color(0xff262626),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
