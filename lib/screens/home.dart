8a2e32847c9eb8c54cd4c9862f3576d7bcf255dd
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-30 18:08:28 +0100

    perf: reduced api data requests and increased app velocity

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:github/components/load.dart';
 import 'package:github/components/repository_container.dart';
 import 'package:http/http.dart' as http;
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
@@ -12,15 +13,32 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final String url = 'https://api.github.com/users/femoraes0';
+  Map userData;
+  List repositories;
 
-  Future<Map> getUserData() async {
-    dynamic response = await http.get(this.url);
-    return json.decode(response.body);
+  @override
+  void initState() {
+    this.userData = null;
+    this.repositories = null;
+    this.getData();
+    super.initState();
   }
 
-  Future<List> getRepositories(String url) async {
-    dynamic response = await http.get(url);
-    return json.decode(response.body);
+  Future<void> getData() async {
+    dynamic responseUser = await http.get(this.url);
+    Map userData = json.decode(responseUser.body);
+
+    print(userData);
+
+    dynamic responseRepos = await http.get(userData['repos_url']);
+    List repositories = json.decode(responseRepos.body);
+
+    print(repositories);
+
+    setState(() {
+      this.userData = userData;
+      this.repositories = repositories;
+    });
   }
 
   @override
@@ -28,222 +46,200 @@ class _HomeState extends State<Home> {
     ThemeData theme = Theme.of(context);
     Size screen = MediaQuery.of(context).size;
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          FeatherIcons.github,
-          size: 28.0,
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Icon(
-              FeatherIcons.search,
-              size: 28.0,
+    return (this.userData != null || this.repositories != null)
+        ? Scaffold(
+            appBar: AppBar(
+              leading: Icon(
+                FeatherIcons.github,
+                size: 28.0,
+              ),
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Icon(
+                    FeatherIcons.search,
+                    size: 28.0,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      body: FutureBuilder(
-          future: this.getUserData(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                print('snapshot');
-                print(snapshot.data);
-                return SingleChildScrollView(
-                  child: Container(
-                    width: screen.width,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 5.0,
-                                horizontal: 10.0,
-                              ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5.0),
-                                color: theme.primaryColor,
-                              ),
-                              child: Text(
-                                'Follow',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15.0,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              height: screen.width * 0.40,
-                              width: screen.width * 0.40,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(180),
-                                color: Colors.redAccent,
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                    snapshot.data['avatar_url'],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 5.0,
-                                horizontal: 10.0,
-                              ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5.0),
-                                color: Color.fromRGBO(224, 105, 198, 1.0),
-                              ),
-                              child: Text(
-                                'Sponsor',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15.0,
-                                ),
-                              ),
-                            ),
-                          ],
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 10.0,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 10.0,
-                            bottom: 3.0,
-                          ),
-                          child: Text(
-                            snapshot.data['name'],
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: theme.primaryColor,
                         ),
-                        Text(
-                          snapshot.data['login'],
+                        child: Text(
+                          'Follow',
                           style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.white54,
-                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                            fontSize: 15.0,
                           ),
+                        ),
+                      ),
+                      Container(
+                        height: screen.width * 0.40,
+                        width: screen.width * 0.40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(180),
+                          color: Colors.redAccent,
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              this.userData['avatar_url'],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color.fromRGBO(224, 105, 198, 1.0),
+                        ),
+                        child: Text(
+                          'Sponsor',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10.0,
+                      bottom: 3.0,
+                    ),
+                    child: Text(
+                      this.userData['name'],
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    this.userData['login'],
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white54,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 8.0,
+                      bottom: 4.0,
+                    ),
+                    child: Text(
+                      this.userData['bio'],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FeatherIcons.mapPin,
+                        color: Colors.white,
+                        size: 15.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          right: 8.0,
+                          left: 3.0,
+                        ),
+                        child: Text(
+                          this.userData['location'],
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Text(
+                      this.userData['blog'],
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: screen.width,
+                    padding: const EdgeInsets.only(
+                      bottom: 5.0,
+                    ),
+                    margin: const EdgeInsets.only(
+                      left: 20.0,
+                      top: 20.0,
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          FeatherIcons.folder,
+                          size: 22,
+                          color: Colors.white,
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(
-                            top: 8.0,
-                            bottom: 4.0,
-                          ),
+                          padding: const EdgeInsets.only(left: 10.0),
                           child: Text(
-                            snapshot.data['bio'],
+                            'Repositories',
                             style: TextStyle(
+                              fontSize: 22.0,
                               color: Colors.white,
-                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
                             ),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              FeatherIcons.mapPin,
-                              color: Colors.white,
-                              size: 15.0,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                right: 8.0,
-                                left: 3.0,
-                              ),
-                              child: Text(
-                                snapshot.data['location'],
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5.0),
-                          child: Text(
-                            snapshot.data['blog'],
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        FutureBuilder(
-                          future: this.getRepositories(
-                            snapshot.data['repos_url'],
-                          ),
-                          builder: (context, snapshot) {
-                            switch (snapshot.connectionState) {
-                              case ConnectionState.done:
-                                print(snapshot.data);
-                                return Container(
-                                  margin: const EdgeInsets.only(top: 20.0),
-                                  padding: const EdgeInsets.all(20.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 5.0),
-                                        child: Text(
-                                          'Repositories',
-                                          style: TextStyle(
-                                            fontSize: 18.0,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w300,
-                                          ),
-                                        ),
-                                      ),
-                                      ListView.builder(
-                                        shrinkWrap: true,
-                                        physics:
-                                            const NeverScrollableScrollPhysics(),
-                                        itemCount: snapshot.data.length,
-                                        itemBuilder: (context, index) {
-                                          Map element = snapshot.data[index];
-                                          return RepositoryContainer(
-                                            name: element['name'],
-                                            description: element['description'],
-                                            stars: element['stargazers_count']
-                                                .toString(),
-                                            forks: element['forks'].toString(),
-                                            language: element['language'],
-                                          );
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              case ConnectionState.waiting:
-                              default:
-                                return Container();
-                            }
-                          },
                         ),
                       ],
                     ),
                   ),
-                );
-              case ConnectionState.waiting:
-              default:
-                return SafeArea(
-                  child: Center(
-                    child: CircularProgressIndicator(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: this.repositories.length,
+                      itemBuilder: (context, index) {
+                        Map element = this.repositories[index];
+                        return RepositoryContainer(
+                          name: element['name'],
+                          description: element['description'],
+                          stars: element['stargazers_count'].toString(),
+                          forks: element['forks'].toString(),
+                          language: element['language'],
+                        );
+                      },
+                    ),
                   ),
-                );
-            }
-          }),
-    );
+                ],
+              ),
+            ),
+          )
+        : Load();
   }
 }
